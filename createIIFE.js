cdeb352de058d48790b893905270ff8c7ada5e79
Bruce Anderson <anderson.bruce.b@gmail.com>
2020-04-05 12:24:43 -0400

    ...

--- createIIFE.js
+++
@@ -1,7 +0,0 @@
-//@ts-check
-const jiife = require('jiife');
-const xl = 'node_modules/xtal-latx/';
-jiife.processFiles([xl + 'define.js', xl + 'qsa.js', xl + 'downloadJSFilesInParallelButLoadInSequence.js', xl + 'xtal-latx.js', 'xtal-treant.js'], 'xtal-treant.iife.js');
-
-
-
